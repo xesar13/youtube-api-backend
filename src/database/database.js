749dd16c441a37cff9a4c271429defd65cfe188d
Cesar Mendivil <mendivilrubioc@gmail.com>
2024-12-08 05:30:40 +0000

    update gitignore

--- src/database/database.js
+++ src/database/database.js
@@ -1,7 +1,7 @@
 const sqlite3 = require('sqlite3').verbose();
 const path = require('path');
 
-const dbPath = path.resolve(__dirname, 'users.db');
+const dbPath = path.resolve(__dirname, '../data/users.db');
 const db = new sqlite3.Database(dbPath);
 
 db.serialize(() => {
